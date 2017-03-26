b957ec547e0867403fa82a3093c9d09c5beb3e60
Mike Moody <amvmoody@gmail.com>
2017-03-26 01:35:27 -0700

    Upated options and pushed the setup further.

--- setup.sh
+++ setup.sh
@@ -6,44 +6,47 @@ if (( $EUID != 0 )); then
 fi
 
 
-if [ ! -f /usr/bin/hifi ]; then
+if [ ! -f /opt/hifi ]; then
     echo "Please install into a new install"
     exit
 fi
 
-function handelgitclone {
+function getRepo {
 
-# Get new tags from remote
 git fetch --tags
 
-# Get latest tag name
-LATESTTAG=$(git describe --tags `git rev-list --tags --max-count=1`)
+LATEST=$(git describe --abbrev=0 --tags)
 
-# Checkout latest tag
-echo "checking out " $LATESTTAG
-git checkout $LATESTTAG
+echo "checking out " $LATEST
+sleep 2
+git checkout $LATEST
 
 }
 
-# list of needed software yum will handel the installing
 
-ech0 "Updating system if needed."
-sleep 2
+
 yum update -y
-echo "Installing epel repo."
-sleep 2
+
 yum install -y epel-release
 yum install -y --enablerepo=epel
-echo "Installing dependencies."
-sleep 2
+
+
+
 yum groupinstall -y "development tools"
 yum openssl-devel -y cmake3 glew-devel git wget libXmu-* libXi-devel libXrandr libXrandr-devel qt5-qt*
 
 
-##create the user hifi
 id -u hifi &>/dev/null || useradd hifi
+id -g hifi &>/dev/null || groupadd hifi
 
+
+mkdir -p /opt/hifi/live
+mkdir -p /opt/hifi/build
 mkdir -p /opt/hifi/source
+mkdir -p /opt/hifi/backups
+mkdir -p /opt/hifi/logs
+
+
 
 
 cd /opt/hifi
