ae93b5f1d0f9cb9d84068d08d8aebb2c2f577518
Mike Moody <amvmoody@gmail.com>
2017-03-27 00:33:31 -0700

    Finished up the alpha version time to test all features and see if it works :D

--- setup.sh
+++ setup.sh
@@ -11,19 +11,6 @@ if [ ! -f /opt/hifi ]; then
     exit
 fi
 
-function getRepo {
-
-git fetch --tags
-
-LATEST=$(git describe --abbrev=0 --tags)
-
-echo "checking out " $LATEST
-sleep 2
-git checkout $LATEST
-
-}
-
-
 
 yum update -y
 
@@ -36,17 +23,51 @@ yum groupinstall -y "development tools"
 yum openssl-devel -y cmake3 glew-devel git wget libXmu-* libXi-devel libXrandr libXrandr-devel qt5-qt*
 
 
-id -u hifi &>/dev/null || useradd hifi
-id -g hifi &>/dev/null || groupadd hifi
 
 
-mkdir -p /opt/hifi/live
-mkdir -p /opt/hifi/build
-mkdir -p /opt/hifi/source
-mkdir -p /opt/hifi/backups
-mkdir -p /opt/hifi/logs
+function doInstall() {
+    id -u hifi &>/dev/null || useradd hifi
+    id -g hifi &>/dev/null || groupadd hifi
+
+    HIFIBASEDIR="/opt/hifi"
+    function setPerms()  {
+      if [ -d "$HIFIBASEDIR/live" ]; then
+        chown -R hifi:hifi $HIFIBASEDIR/live
+      fi
+    }
+    if [ ! -f /opt/hifi ]; then
+        echo "Already Installed."
+        exit
+    fi
+
+    mkdir -p /opt/hifi/live
+    mkdir -p /opt/hifi/build
+    mkdir -p /opt/hifi/source
+    mkdir -p /opt/hifi/backups
+    mkdir -p /opt/hifi/logs
+
+    cd $HIFIBASEDIR/source
+    git fetch --tags
+    LATEST=$(git describe --abbrev=0 --tags)
+    git checkout $LATEST
+
+    cmake3 -B$HIFIBASEDIR/build -DGET_LIBOVR=1
+    cd -
+    cd $HIFIBASEDIR/build
+
+    make domain-server && make assignment-client
+    cd -
 
 
+    cp $HIFIBASEDIR/build/hifi.service /etc/systemd/system/hifi.service
+    cp $HIFIBASEDIR/build/* $HIFIBASEDIR/live
+    cp $HIFIBASEDIR/build/hifi /usr/local/bin/hifi
 
 
-cd /opt/hifi
+    setPerms
+    systemctl enable hifi.service
+    systemctl start hifi.service
+}
+
+
+doInstall
